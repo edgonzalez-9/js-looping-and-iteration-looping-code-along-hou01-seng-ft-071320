8ccb92d1aa3c6d1bd859a0705376af838154bf02
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2020-08-25 16:39:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let thankYou = []
+    for(let c = 0; c < names.length; c++){
+        thankYou.push(`Thank you, ${names[c]}, for the wonderful ${event} gift!`);
+    }
+    return thankYou
+}
+
+function countDown(number){
+    while(number > 0){
+        console.log(number)
+        number -= 1
+    }
+    console.log(number)
+}
